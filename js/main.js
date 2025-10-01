c2675589de4bac440502104d589425a7bd653a68
Riku200 <mimimimi20130902@gmail.com>
2025-10-02 00:24:47 +0900

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,12 @@
+// Custom Scripts
+
+// Example: Add a smooth scroll effect for anchor links
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
